b88db2ac0ca4b814a63ecab307ceb39f578f96c2
RONALDBUZAGLO <senseironald@gmail.com>
2021-02-25 15:47:13 -0400

    primeiro commit

---
+++ app.js
@@ -0,0 +1,25 @@
+//declaração e configuração do express
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//Set do ejs como engine
+app.set('view engine','ejs');
+
+//Configurar pasta de arquivos estáticos
+//super importante: o EJS não funciona se a estrutura de 
+//pastas não estiver correta
+app.use(express.static('public'));
+
+//Rotas
+app.get('/', (req, res) => {
+    res.render('pages/index');
+});
+
+app.get('/teste',(req, res)=>{
+    res.render('pages/about');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
